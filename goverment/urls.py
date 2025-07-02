f9a7183a10737051b1ea0e7f89cb76af4e6a2396
nitishjha74 <kumarnitishjha8877@gmail.com>
2025-07-02 19:08:59 +0530

    total_placed_student

--- goverment/urls.py
+++ goverment/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("jobs-list/", views.JobListGovermentAPIView.as_view(), name="job-list"),
     path('applications/<int:job_id>/', views.JobApplicationListOfStudentGoverment.as_view(), name='job-applications-by-job'),
     path("government/dashboard/", views.DashboardSummaryAPIView.as_view(), name="dashboard-summary"),
-    
+    path('placed-students/', views.PlacedStudentListAPIView.as_view(), name='placed-student-list'),
     path('job/count-by-business/', views.JobCountByBusinessAPIView.as_view()),
     
 ]
