cdb212884627e5dad1cc4f13c148ff5fbc399967
nitishjha74 <kumarnitishjha8877@gmail.com>
2025-07-23 18:54:21 +0530

    views_documents_business

--- jobcard_business/urls.py
+++ jobcard_business/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('employer/dashboard/', views.EmployerDashboardAPIView.as_view(), name='employer-dashboard'),
     path('job-details/<int:job_id>/', views.JobDetailBusinessAPIView.as_view(), name='job-details'),
     path('list/student/<int:job_id>/', views.JobApplicationListBusinessAPI.as_view(), name='business-job-applications'),
-    
+    path('documents/details/<int:card_number>/', views.GetMemberDocumentsAPIView.as_view(), name='get-member-documents'),
     
     path('institution-jobs/', institute_api.JobListInstituteAPI.as_view(), name='institution-job-list'),
     path('applied/student/<int:job_id>/', institute_api.JobApplicationListInstituteAPIView.as_view(), name='Institution-job-applications'),
